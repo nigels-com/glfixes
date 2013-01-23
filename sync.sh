67b37e62a1367a8278b4e39b33e1c884451827c5
Nigel Stewart <nigels@users.sourceforge.net>
2013-01-23 12:11:09 -0600

    Preserve more upstream directory structure, add GLSL documentation.

--- sync.sh
+++ sync.sh
@@ -4,12 +4,15 @@
 # Extension specifications, .spec and .tm files
 #
 
-wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=2 --accept=txt,spec,tm \
+wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=1 --accept=txt,spec,tm,h \
   http://www.opengl.org/registry/
 
 #
-# OpenGL Software Development Kit - manual pages
+# OpenGL Software Development Kit - manual pages for GL and GLSL
 #
 
 wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=1 --accept=html,xml \
   http://www.opengl.org/sdk/docs/man4/xhtml/
+
+wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=1 --accept=html,xml \
+  http://www.opengl.org/sdk/docs/manglsl/xhtml/
