7a12be89e5ba5e8ef21768c0e264f9094c1b512a
Nigel Stewart <nigels@users.sourceforge.net>
2013-01-23 11:45:27 -0600

    Initial opengl.org snapshot of http://www.opengl.org/sdk/docs/man4/

--- sync.sh
+++ sync.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-cd gl && \
-  wget --mirror --no-parent --no-host-directories --cut-dirs=2 --accept=txt,spec,tm \
-    http://www.opengl.org/registry/
 
+#
+# Extension specifications, .spec and .tm files
+#
+
+wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=2 --accept=txt,spec,tm \
+  http://www.opengl.org/registry/ && \
+
+#
+# OpenGL Software Development Kit - manual pages
+#
+
+wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=1 --accept=html,xml \
+  http://www.opengl.org/sdk/docs/man4/xhtml/
