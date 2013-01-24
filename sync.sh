541273ed4d180207726ce0fb8fadf3d9b506cf80
Nigel Stewart <nigels@users.sourceforge.net>
2013-01-23 19:43:41 -0600

    Also mirror GL ES headers and extension specifications.

--- sync.sh
+++ sync.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 #
-# Extension specifications, .spec and .tm files
+# Desktop OpenGL
 #
+
+# Extension specifications, .spec and .tm files
 
 wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=1 --accept=txt,spec,tm,h \
   http://www.opengl.org/registry/
 
-#
 # OpenGL Software Development Kit - manual pages for GL and GLSL
-#
 
 wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=1 --accept=html,xml \
   http://www.opengl.org/sdk/docs/man4/xhtml/
@@ -20,3 +20,15 @@ wget -P gl --mirror --no-parent --no-host-directories --cut-dirs=1 --accept=html
 # Tidy-up
 
 rm gl/robots.txt
+
+#
+# OpenGL ES
+#
+
+wget -P gles --mirror --no-parent --no-host-directories --cut-dirs=2 --accept=txt,spec,tm,h \
+  http://www.khronos.org/registry/gles/
+
+# Tidy-up
+
+rm gles/robots.txt
+
