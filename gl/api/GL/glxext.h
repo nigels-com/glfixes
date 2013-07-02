26ab6f0d0b60e9b5dc7ef859005ea264433bdb92
Nigel Stewart <nigels@users.sourceforge.net>
2013-07-02 10:45:20 -0500

    opengl.org update 2013-06-25

--- gl/api/GL/glxext.h
+++ gl/api/GL/glxext.h
@@ -36,7 +36,7 @@ extern "C" {
 ** Khronos $Revision$ on $Date$
 */
 
-#define GLX_GLXEXT_VERSION 20130623
+#define GLX_GLXEXT_VERSION 20130624
 
 /* Generated C header for:
  * API: glx
--- gl/api/GL/wglext.h
+++ gl/api/GL/wglext.h
@@ -41,7 +41,7 @@ extern "C" {
 #include <windows.h>
 #endif
 
-#define WGL_WGLEXT_VERSION 20130623
+#define WGL_WGLEXT_VERSION 20130624
 
 /* Generated C header for:
  * API: wgl
