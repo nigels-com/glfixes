82c557bf3a766747472f44b24b3fb8a895ee2acd
Nigel Stewart <nigel.stewart@immersaview.com>
2014-08-10 12:01:49 +1000

    opengl.org update 2014-08-10

--- egl/api/EGL/egl.h
+++ egl/api/EGL/egl.h
@@ -33,12 +33,12 @@ extern "C" {
 ** used to make the header, and the header can be found at
 **   http://www.opengl.org/registry/
 **
-** Khronos $Revision: 27553 $ on $Date: 2014-07-29 22:49:34 -0700 (Tue, 29 Jul 2014) $
+** Khronos $Revision: 27652 $ on $Date: 2014-08-08 15:34:23 -0700 (Fri, 08 Aug 2014) $
 */
 
 #include <EGL/eglplatform.h>
 
-/* Generated on date 20140729 */
+/* Generated on date 20140808 */
 
 /* Generated C header for:
  * API: egl
